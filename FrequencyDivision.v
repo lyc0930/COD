5c9c987f7e96705ecde8a19b0f53574d5472eae4
罗晏宸 <yc1023601986@mail.ustc.edu.cn>
2019-04-10 22:45:51 +0800

    上传新文件

---
+++ FrequencyDivision.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2019/04/10 21:57:25
+// Design Name: 
+// Module Name: FrequencyDivision
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FrequencyDivision(input Origin_Clock, input reset, output pulse);
+	 reg [23:0] count;
+     always @(posedge Origin_Clock or posedge reset)
+     begin
+     	if (reset)
+     		count <= 'd0;  		
+		else 
+		begin
+			if(count >= 'd4999999)
+     			count	<= 'd0;
+     		else
+     			count	<= count + 'd1;
+     	end
+     end
+     assign	pulse =  (count == 'd2999999) ? 1'b1 : 1'b0;
+endmodule
